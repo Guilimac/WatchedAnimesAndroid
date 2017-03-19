7b24a14d487acf74bc28310b8f8c1a43d7e242e6
Guilherme <guilherme@ztrade.com>
2017-03-19 15:02:32 -0300

    Estrutura do projeto e tela de login e splashscreen

---
+++ app/src/main/java/br/com/fiap/watchedanimesandroid/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package br.com.fiap.watchedanimesandroid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        carregar();
+    }
+
+    private void carregar() {
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Após o tempo definido irá executar a próxima tela
+                Intent intent = new Intent(SplashScreenActivity.this,
+                        LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
